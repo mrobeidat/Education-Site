c2595027c90f414ed922802b4a2022e7ccab2989
Yousef Obeidat <y.linux96@gmail.com>
2022-03-21 01:33:01 +0300

    responsive nav

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -10,8 +10,26 @@ const Navbar = () => {
       setNavbar(false);
     }
   };
+
   useEffect(() => {
     window.addEventListener("scroll", onScroll);
+
+    const menu = document.querySelector(".nav__menu");
+    const openBtn = document.querySelector("#open-menu-btn");
+    const closeBtn = document.querySelector("#close-menu-btn");
+    openBtn.addEventListener("click", () => {
+      menu.style.display = "flex";
+      closeBtn.style.display = "inline-block";
+      openBtn.style.display = "none";
+    });
+
+    const closeNav = () => {
+      menu.style.display = "none";
+      closeBtn.style.display = "none";
+      openBtn.style.display = "inline-block";
+    };
+
+    closeBtn.addEventListener("click", closeNav);
   });
 
   return (
