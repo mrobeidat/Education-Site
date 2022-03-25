d245cd67a12e15007909b5ff9450ae5f7263f232
Yousef Obeidat <y.linux96@gmail.com>
2022-03-25 14:14:14 +0300

    add content to courses page

--- pages/Courses.js
+++ pages/Courses.js
@@ -57,7 +57,39 @@ const Header = () => {
             <div className="course__image">
               <Image
                 alt="img"
-                src="/assests/course1.jpg"
+                src="/assests/course2.jpg"
+                width={450}
+                height={300}
+              />
+            </div>
+            <div className="course__info">
+              <h4>Responsive Social Media Website UI Design</h4>
+              <p>
+                Officia sit ea ex sint cupidatat ipsum elit consequat
+                consectetur sint. Ut consequat occaecat mollit in eiusmod
+                eiusmod id sint.
+              </p>
+            </div>
+            <Link href="/Courses">
+              <a href="" className="btn btn-primary">
+                Learn More
+              </a>
+            </Link>
+          </article>  <article
+            data-aos="fade-down"
+            data-aos-offset="50"
+            data-aos-delay="100"
+            data-aos-duration="1000"
+            data-aos-easing="ease-in-out"
+            data-aos-mirror="true"
+            data-aos-once="true"
+            data-aos-anchor-placement="top"
+            className="course"
+          >
+            <div className="course__image">
+              <Image
+                alt="img"
+                src="/assests/course3.jpg"
                 width={450}
                 height={300}
               />
@@ -121,7 +153,7 @@ const Header = () => {
             <div className="course__image">
               <Image
                 alt="img"
-                src="/assests/course1.jpg"
+                src="/assests/course4.jpg"
                 width={450}
                 height={300}
               />
@@ -153,7 +185,7 @@ const Header = () => {
             <div className="course__image">
               <Image
                 alt="img"
-                src="/assests/course1.jpg"
+                src="/assests/course5.jpg"
                 width={450}
                 height={300}
               />
@@ -185,7 +217,7 @@ const Header = () => {
             <div className="course__image">
               <Image
                 alt="img"
-                src="/assests/course1.jpg"
+                src="/assests/course5.jpg"
                 width={450}
                 height={300}
               />
@@ -217,7 +249,7 @@ const Header = () => {
             <div className="course__image">
               <Image
                 alt="img"
-                src="/assests/course1.jpg"
+                src="/assests/course6.jpg"
                 width={450}
                 height={300}
               />
@@ -249,7 +281,7 @@ const Header = () => {
             <div className="course__image">
               <Image
                 alt="img"
-                src="/assests/course1.jpg"
+                src="/assests/course7.jpg"
                 width={450}
                 height={300}
               />
@@ -281,7 +313,7 @@ const Header = () => {
             <div className="course__image">
               <Image
                 alt="img"
-                src="/assests/course1.jpg"
+                src="/assests/course8.jpg"
                 width={450}
                 height={300}
               />
@@ -313,7 +345,7 @@ const Header = () => {
             <div className="course__image">
               <Image
                 alt="img"
-                src="/assests/course1.jpg"
+                src="/assests/course9.jpg"
                 width={450}
                 height={300}
               />
@@ -345,7 +377,7 @@ const Header = () => {
             <div className="course__image">
               <Image
                 alt="img"
-                src="/assests/course1.jpg"
+                src="/assests/course10.jpg"
                 width={450}
                 height={300}
               />
@@ -377,7 +409,7 @@ const Header = () => {
             <div className="course__image">
               <Image
                 alt="img"
-                src="/assests/course1.jpg"
+                src="/assests/course11.jpg"
                 width={450}
                 height={300}
               />
@@ -409,7 +441,7 @@ const Header = () => {
             <div className="course__image">
               <Image
                 alt="img"
-                src="/assests/course1.jpg"
+                src="/assests/course12.jpg"
                 width={450}
                 height={300}
               />
@@ -441,7 +473,7 @@ const Header = () => {
             <div className="course__image">
               <Image
                 alt="img"
-                src="/assests/course1.jpg"
+                src="/assests/course13.jpg"
                 width={450}
                 height={300}
               />
@@ -473,7 +505,7 @@ const Header = () => {
             <div className="course__image">
               <Image
                 alt="img"
-                src="/assests/course1.jpg"
+                src="/assests/course14.jpg"
                 width={450}
                 height={300}
               />
@@ -505,39 +537,7 @@ const Header = () => {
             <div className="course__image">
               <Image
                 alt="img"
-                src="/assests/course1.jpg"
-                width={450}
-                height={300}
-              />
-            </div>
-            <div className="course__info">
-              <h4>Responsive Social Media Website UI Design</h4>
-              <p>
-                Officia sit ea ex sint cupidatat ipsum elit consequat
-                consectetur sint. Ut consequat occaecat mollit in eiusmod
-                eiusmod id sint.
-              </p>
-            </div>
-            <Link href="/Courses">
-              <a href="" className="btn btn-primary">
-                Learn More
-              </a>
-            </Link>
-          </article>  <article
-            data-aos="fade-down"
-            data-aos-offset="50"
-            data-aos-delay="100"
-            data-aos-duration="1000"
-            data-aos-easing="ease-in-out"
-            data-aos-mirror="true"
-            data-aos-once="true"
-            data-aos-anchor-placement="top"
-            className="course"
-          >
-            <div className="course__image">
-              <Image
-                alt="img"
-                src="/assests/course1.jpg"
+                src="/assests/course15.jpg"
                 width={450}
                 height={300}
               />
@@ -571,7 +571,7 @@ const Header = () => {
             <div className="course__image">
               <Image
                 alt="img"
-                src="/assests/course3.jpg"
+                src="/assests/course16.jpg"
                 width={450}
                 height={300}
               />
