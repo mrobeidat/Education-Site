6a139b8431374767d1a6754b760213371e0d3bb7
Yousef Obeidat <y.linux96@gmail.com>
2022-04-05 18:19:19 +0300

    add loading spinner

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,34 @@
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import Head from 'next/head'
-
 import "../styles/global.scss";
+import { SpinnerDotted } from 'spinners-react';
+import React, { useEffect, useState } from "react";
 function MyApp({ Component, pageProps }) {
+  
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3500);
+  }, []);
   return (
     <>
 
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       </Head>
+      {loading ? (
+        <SpinnerDotted className="spinner" size={200} thickness={45} speed={121} color="rgba(57, 172, 169, 1)" secondaryColor="rgba(57, 107, 172, 1)" />
+      ) : (
+      <div className="main__div">
       <Navbar />
 
       <Component {...pageProps} />
       <Footer />
+      </div>
+      )}
     </>
   );
 }
